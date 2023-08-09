6dd80e25c3d4c1be3401925e0ff2270b9c7e1d2c
Anika <tabassumanika210@gmail.com>
2023-08-09 19:44:27 +0600

    edited vercel

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT||5000;
 //midddleware
-app.use(cors());
+// app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+
+
+
 app.use(express.json());
 
-console.log(process.env.DB_PASS)
+// console.log(process.env.DB_PASS)
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ashfkhm.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://Nyama:o7KnomPSY1XNPMPm@cluster0.ashfkhm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,12 +30,27 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+//   // useNewUrlParser: true,
+//   // useUnifiedTopology: true,
+//   // maxPoolSize: 10,
+// });
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     //mongodb
+    client.connect((error)=>{
+      if(error){
+        console.log(error)
+        return;
+      }
+    });
     const catagoryCollection = client.db("Nyama").collection("CatagoryCard");
     const ADDaToyCollection = client.db("Nyama").collection("ADDaToy");
 
@@ -68,6 +93,20 @@ async function run() {
       res.send(result);
 
      })
+     app.get('/AddAtoy/:id', async(req, res) => {
+      const id= req.params.id;
+      const query={ _id: new ObjectId(id)};
+      
+      const options = {
+       
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { picture:1, name:1, SellerName:1, SellerEmail:1, price:1, Ratings:1, Stock:1,Description:1  },
+      };
+
+
+      const result=await ADDaToyCollection.findOne(query,options) ;
+      res.send(result);
+     })
 
      //send data from client to server data
      app.post('/AddAtoy', async(req, res) => {
@@ -105,7 +144,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
